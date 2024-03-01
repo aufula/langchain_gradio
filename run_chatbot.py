f1e7bf833205848651ed1f8966e4a58738ced36b
shaotian.hst <shaotian.hst@alibaba-inc.com>
2024-03-01 18:15:52 +0800

    ok1

--- run_chatbot.py
+++ run_chatbot.py
@@ -4,5 +4,4 @@ from dotenv import load_dotenv, find_dotenv
 
 if __name__ == '__main__':
     load_dotenv(find_dotenv())
-    reloader = hupper.start_reloader('chatbot.main')  # Replace 'your_gradio_app.main' with the function that launches your Gradio app
     chatbot.main()
